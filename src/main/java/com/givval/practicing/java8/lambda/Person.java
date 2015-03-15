08637b05b4c4a09a0764232149b914274c5d3a85
hanscoder <hanssowa@gmail.com>
2015-03-15 21:09:59 +0100

    Provide a test for an empty implementation.

---
+++ src/main/java/com/givval/practicing/java8/lambda/Person.java
@@ -0,0 +1,11 @@
+package com.givval.practicing.java8.lambda;
+
+public class Person {
+    public Person(String firstname, String familyname) {
+
+    }
+
+    public boolean hasSameName(Person aPerson) {
+        throw new IllegalStateException("Please provide an implementation.");
+    }
+}
---
+++ src/test/java/com/givval/practicing/java8/lambda/PersonTest.java
@@ -0,0 +1,33 @@
+package com.givval.practicing.java8.lambda;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Provide an implementation using Lambda.
+ */
+public class PersonTest {
+
+    @Test
+    public void should_have_same_name() {
+        Person hansInVienna = new Person("Hans", "Towa");
+        Person hansInGermany = new Person("Hans", "Towa");
+
+        boolean isSameName = hansInVienna.hasSameName(hansInGermany);
+
+        assertThat(isSameName, is(equalTo(true)));
+    }
+
+    @Test
+    public void should_have_different_name() {
+        Person hansInVienna = new Person("Hans", "Towa");
+        Person hansInGermany = new Person("Hans", "Bowa");
+
+        boolean isSameName = hansInVienna.hasSameName(hansInGermany);
+
+        assertThat(isSameName, is(equalTo(false)));
+    }
+}
